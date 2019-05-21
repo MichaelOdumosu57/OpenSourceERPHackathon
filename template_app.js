c82cbdc945457f645c954373e8b12d7ee9643d1d
Michael Odumosu <michaelodumosu57@gmail.com>
2019-05-21 09:22:59 -0400

    want to see if  I can retieve a php worth object from the php backend

--- template_app.js
+++ template_app.js
@@ -17,24 +17,25 @@ var rateIdString;
  // }  /**/
 
 
+app.get('/backend/nodeBackend/object/:obj', function (req, res, next) {
+    console.log(   req.params   )
+})
 
 
 app.get('/backend/nodeBackend/:rate_id', function (req, res, next) {
-    var error = err
+    
+    
+    /*enabling the API to access needed items*/ //{
     var response = res
-    var request = req
-	ultraObject.reqBody({
-		stream:req,
-		fn:function(dev_obj){
-		},
-		keep:'false',
-		finish:function(dev_obj){
-		    console.log(dev_obj.stream.body)
-		    
-		    /*where the rate id is*/ //{
-		    console.log(request.params.rate_id)
-            rateIdString = request.params.rate_id
+    var nexts = next
+    // }  /**/
+    
+    /*if it came in the route params*/ //{
+        if(   req.params.rate_id.indexOf('se-') !== -1   ){
             
+            
+            rateIdString = req.params.rate_id
+            /*the request object to the shipengineAPI for the label response object*/ //{
             var options = {
               "method": "POST",
               "hostname": "api.shipengine.com",
@@ -47,44 +48,82 @@ app.get('/backend/nodeBackend/:rate_id', function (req, res, next) {
                 "postman-token": "8fead3f1-1d3d-eeb8-b646-98c541cff875"
               }
             };
-
-            // console.log( rateIdString )
-            // console.log( options )
             // }  /**/
             
             /*making xhr request to shipengine API*/ //{
-            var req = http.request(options, function (res) {
-              var chunks = [];
+            var req = http.request(options, function (res,next) {
+                var chunks = [];
             
-
-              res.on("data", function (chunk) {
-                chunks.push(chunk);
-              });
+    
+                  res.on("data", function (chunk) {
+                    chunks.push(chunk);
+                  });
             
-              res.on("end", function () {
-                var body = Buffer.concat(chunks);
-                var neededString =  JSON.parse(body.toString()).label_download.hre
-                response.send(neededString)
-              });
+                  /*grabbing and sending the needed shipping label link*/ //{
+                  res.on("end", function () {
+                    var body = Buffer.concat(chunks);
+                    console.log(body.toString())
+                    console.log(   JSON.parse(body.toString()).label_download   )
+                    
+                    /*error handling for shipment API label response object for the label link*/ //{
+                    if(   JSON.parse(body.toString()).label_download !== undefined   ){
+                        
+                        
+                        var neededString =  JSON.parse(body.toString()).label_download.href
+                        response.send(neededString)
+                        
+                        
+                    }
+                    
+                    
+                    else if(   JSON.parse(body.toString()).label_download === undefined || true   ){
+                        
+                        response.send("https://api.shipengine.com/v1/downloads/10/uDlB2a31WkWw6DUxsoMo8w/label-380264.pdf")
+                        // return
+                        //so appartlent i need to return mabye specifically this string for it to work
+                        
+                    }
+                    // }  /**/
+                    
+                  });
+                  // }  /**/
               
             });
+            console.log(req.end());
             
-            // req.write("\n  {\n    \"label_format\":\"pdf\",\n    \"label_layout\": \"4x6\",\n    \"label_download_type\": \"download\"\n  }");
-            req.end();
             
             // }  /**/
-            
-            
-            
-            
-		}
-	})
-		    
+        }
+    // }  /**/
+
+    /*if it might came in the body*/ //{
+    if(   req.params.rate_id.indexOf('se-') === -1   ){
+    	
+    	
+    	ultraObject.reqBody({
+    		stream:req,
+    		fn:function(dev_obj){
+    		},
+    		keep:'false',
+    		finish:function(dev_obj){
+    		    
+    		    rateIdString = dev_obj.stream.body
+                if(   dev_obj.stream.body.indexOf('se-') !== -1   ){
+        // 		console.log(rateIdString)
+                }
+        		
+    		}
+    	})
+    	
+    	
+    }
+    // }  /**/
+    
 		    
 
 },function (err,req, res, next) {
-        console.log(err)
-        res.send("https://api.shipengine.com/v1/downloads/10/uDlB2a31WkWw6DUxsoMo8w/label-380264.pdf")
+        // console.log('this',err)
+        // res.send("https://api.shipengine.com/v1/downloads/10/uDlB2a31WkWw6DUxsoMo8w/label-380264.pdf")
     });
 
 
