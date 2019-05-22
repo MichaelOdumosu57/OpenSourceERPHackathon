4e9ce58b1909fccab0f8c0cfa4129c3db8166c31
Michael Odumosu <michaelodumosu57@gmail.com>
2019-05-21 22:53:37 -0400

    able to receive an object from other API but the object is broken i  must fix somehow

--- template_app.js
+++ template_app.js
@@ -11,16 +11,72 @@ const http = require("https");
 app.use(cors())
 app.use(compression())
 
-
 /*adding the dev_obj to args*/ //{
 var rateIdString;
  // }  /**/
 
 
-app.get('/backend/nodeBackend/object/:obj', function (req, res, next) {
+app.post('/backend/nodeBackend/object/:obj', function (req, res, next) {
     console.log(   req.params   )
     console.log(   req.query   )
-    res.send(    req.query   )
+    console.log(   req.body   )
+    
+    /*enabling the API to access needed items*/ //{
+    var response = res
+    var nexts = next
+    // }  /**/
+    
+    
+    
+    /*enabling the API to access needed items*/ //{
+	ultraObject.reqBody({
+		stream:req,
+		fn:function(dev_obj){
+		},
+		keep:'false',
+		finish:function(dev_obj){
+		    
+		    console.log(   dev_obj.stream.body   )
+		    response.send("https://api.shipengine.com/v1/downloads/10/uDlB2a31WkWw6DUxsoMo8w/label-380264.pdf")
+		    console.log(   JSON.parse(dev_obj.stream)   )
+    		
+		}
+	})
+    // }  /**/
+
+    // /* setup http request*/ //{
+    var options = {
+      "method": "POST",
+      "hostname": "api.shipengine.com",
+      "port": null,
+      "path": "/v1/labels",
+      "headers": {
+        "content-type": "application/json",
+        "api-key": "TEST_bAy47gWWvwWCzHPnXEp5V083hhztc5lDnGbLtAeBp7U",
+        "cache-control": "no-cache",
+        "postman-token": "d62e8ed8-fcd9-beeb-af92-00bf9b3cce0b"
+      }
+    };
+    // // }  /**/
+
+    var req = http.request(options, function (res) {
+      var chunks = [];
+    
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+    
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+        response.send(    JSON.parse(body.toString()).label_download.href  )
+      });
+    });
+    
+    req.write(" \n{\n  \"shipment\": {\n    \"service_code\": \"usps_priority_mail\",\n    \"ship_to\": {\n      \"name\": \"Mickey and Minnie Mouse\",\n      \"phone\": \"+1 (714) 781-4565\",\n      \"company_name\": \"The Walt Disney Company\",\n      \"address_line1\": \"500 South Buena Vista Street\",\n      \"city_locality\": \"Burbank\",\n      \"state_province\": \"CA\",\n      \"postal_code\": \"91521\",\n      \"country_code\": \"US\",\n      \"address_residential_indicator\": \"No\"\n    },\n    \"ship_from\": {\n      \"name\": \"Shippy\",\n      \"phone\": \"512-485-4282\",\n      \"company_name\": \"ShipEngine\",\n      \"address_line1\": \"3800 N. Lamar Blvd.\",\n      \"address_line2\": \"Suite 220\",\n      \"city_locality\": \"Austin\",\n      \"state_province\": \"TX\",\n      \"postal_code\": \"78756\",\n      \"country_code\": \"US\",\n      \"address_residential_indicator\": \"No\"\n    },\n    \"packages\": [\n      {\n        \"weight\": {\n          \"value\": 20,\n          \"unit\": \"ounce\"\n        }\n      }\n    ]\n  }\n }");
+    req.end();
+    
+    
 })
 
 
@@ -139,3 +195,10 @@ app.get('/backend/index', function (req, res, next) {
 
 
 app.listen(port, () => console.log(`${file_name} app listening on port ${port}!`))
+
+
+
+
+// how does the shipengineAPI manage to take his goobly gook and turn it to a meaningful object
+
+
